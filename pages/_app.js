b79125c5e0472d5ad1ed115235f0147d36761ec9
pakariisbum <pakariisbum@gmail.com>
2023-05-27 20:34:26 +0000

    world3

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,7 @@ function MyApp({ Component, pageProps }) {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Roboto:wght@300;400;500;700&display=swap"
           rel="stylesheet"
         />
-        <script src="https://cdn.tailwindcss.com"></script>
+        {/* <script src="https://cdn.tailwindcss.com"></script> */}
         <link
           rel="shortcut icon"
           href="assets/images/favicon.png"
