6676edd9d086e719e5e2cd84717236602244eb8a
pakariisbum <pakariisbum@gmail.com>
2023-05-28 16:53:35 +0000

    world3

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,12 +4,25 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
+const { randomBytes } = require("ethers/lib/utils");
 const hre = require("hardhat");
 const { ethers } = require("hardhat");
 
+const priceFeedAddress = 0x0715a7794a1dc8e42615f059dd6e406a6594651a;
+const vrfCoordinator = 0x7a1bac17ccc5b313516c5e16fb24f7659aa5ebed;
+const linkToken = 0x326c977e6efc84e512bb9c30f76e30c160ed06fb;
+const keyHash = 0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f;
+const fee = 100; // Specify the desired fee value
+
 async function main() {
   const World3 = await ethers.getContractFactory("World3");
-  const world3 = await World3.deploy(); // Specify the initial supply here
+  const world3 = await World3.deploy(
+    priceFeedAddress,
+    vrfCoordinator,
+    linkToken,
+    keyHash,
+    fee
+  );
 
   await world3.deployed();
 
