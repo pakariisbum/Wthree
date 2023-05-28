d5840ca8b1c2772ea3535f353b1e80aeaa8b4df3
pakariisbum <pakariisbum@gmail.com>
2023-05-28 16:59:08 +0000

    world3

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,7 +4,6 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-const { randomBytes } = require("ethers/lib/utils");
 const hre = require("hardhat");
 const { ethers } = require("hardhat");
 
