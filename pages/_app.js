bb3f329a47d3cee40f65626ceb10c75f1b9aab20
pakariisbum <pakariisbum@gmail.com>
2023-05-28 22:26:10 +0000

    world3

--- pages/_app.js
+++ pages/_app.js
@@ -12,8 +12,6 @@ import {
 } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
 
-
-
 const xdcApothem = {
   id: 80001,
   name: "Mumbai",
@@ -93,37 +91,39 @@ const myTheme = merge(midnightTheme(), {
   },
 });
 function MyApp({ Component, pageProps }) {
-
   useEffect(() => {
-    changeNetwork
-  }, [])
-  
+    changeNetwork;
+  }, []);
+
   return (
     <Fragment>
-         <WagmiConfig client={wagmiClient}>
-          <RainbowKitProvider chains={chains} theme={myTheme}><
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Roboto:wght@300;400;500;700&display=swap"
-          rel="stylesheet"
-        />
-        {/* <script src="https://cdn.tailwindcss.com"></script> */}
-        <link
-          rel="shortcut icon"
-          href="assets/images/favicon.png"
-          type="image/x-icon"
-        />
-        <link rel="icon" href="assets/images/favicon.png" type="image/x-icon" />
-        <title>
-          Masden || charity & fundrising Responsive React NextJS Template
-        </title>
-      </Head>
-      <Component {...pageProps} />
-      </RainbowKitProvider>
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains} theme={myTheme}>
+          <Head>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link
+              href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Roboto:wght@300;400;500;700&display=swap"
+              rel="stylesheet"
+            />
+            {/* <script src="https://cdn.tailwindcss.com"></script> */}
+            <link
+              rel="shortcut icon"
+              href="assets/images/favicon.png"
+              type="image/x-icon"
+            />
+            <link
+              rel="icon"
+              href="assets/images/favicon.png"
+              type="image/x-icon"
+            />
+            <title>
+              Masden || charity & fundrising Responsive React NextJS Template
+            </title>
+          </Head>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
       </WagmiConfig>
-
     </Fragment>
   );
 }
