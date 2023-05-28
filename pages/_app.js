61f865649485227a2bdccbfb4a81aa6c2edab539
pakariisbum <pakariisbum@gmail.com>
2023-05-28 22:26:06 +0000

    world3

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,107 @@
 import Head from "next/head";
 import { Fragment } from "react";
 import "../styles/globals.css";
+import { useAccount, useConnect, useDisconnect } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  darkTheme,
+  midnightTheme,
+} from "@rainbow-me/rainbowkit";
+import "@rainbow-me/rainbowkit/styles.css";
 
+
+
+const xdcApothem = {
+  id: 80001,
+  name: "Mumbai",
+  network: "Mumbai",
+  nativeCurrency: {
+    decimals: 18,
+    name: "mumbai",
+    symbol: "MATIC",
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://rpc-mumbai.maticvigil.com"],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "Mumbai Explorer",
+      url: "https://mumbai.polygonscan.com",
+    },
+  },
+  testnet: true,
+};
+
+const networks = {
+  theta: {
+    chainId: `0x${Number(80001).toString(16)}`,
+    chainName: "mumbai Testnet",
+    nativeCurrency: {
+      name: "mumbai Testnet",
+      symbol: "MATIC",
+      decimals: 18,
+    },
+    rpcUrls: ["https://rpc-mumbai.maticvigil.com"],
+    blockExplorerUrls: ["https://mumbai.polygonscan.com"],
+  },
+};
+
+const changeNetwork = async () => {
+  try {
+    if (!window.ethereum) throw new Error("No crypto wallet found");
+    await window.ethereum.request({
+      method: "wallet_addEthereumChain",
+      params: [
+        {
+          ...networks["theta"],
+        },
+      ],
+    });
+  } catch (err) {
+    alert(err.message);
+  }
+};
+
+const { provider, chains } = configureChains(
+  [xdcApothem],
+  [
+    // alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
+    publicProvider(),
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "Sustain",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+const myTheme = merge(midnightTheme(), {
+  colors: {
+    accentColor: "#18181b",
+    accentColorForeground: "#fff",
+  },
+});
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    changeNetwork
+  }, [])
+  
   return (
     <Fragment>
+         <WagmiConfig client={wagmiClient}>
+          <RainbowKitProvider chains={chains} theme={myTheme}><
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
@@ -24,6 +121,9 @@ function MyApp({ Component, pageProps }) {
         </title>
       </Head>
       <Component {...pageProps} />
+      </RainbowKitProvider>
+      </WagmiConfig>
+
     </Fragment>
   );
 }
